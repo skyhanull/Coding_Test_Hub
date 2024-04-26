8a4daadba2c77b791efae0afde7d4c98b4a578c9
AnYoonKyoung <106054006+skyhanull@users.noreply.github.com>
2024-04-26 23:37:18 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.79 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,16 @@
+function solution(n, control) {
+    var answer = n;
+     for(const i of control){
+    if(i === "w"){
+        answer += 1
+    }else if(i === "s"){
+      answer -= 1
+    }
+    else if(i === "d"){
+      answer += 10
+    }else{
+     answer -= 10
+    }
+}
+    return answer;
+}
